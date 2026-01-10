93137052bb7c101818020eaa4d9ff05451120ff2
carreiranicolas <nicolascarreira2005@gmail.com>
2026-01-10 17:12:36 -0300

    Feita toda a parte dinamica e persistencia dos dados, agora falta só a parte de atualizar tarefa e excluir tarefa

--- tarefas/views.py
+++ tarefas/views.py
@@ -1,6 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Tarefas
+from .forms import TarefasForm
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'tarefas/home.html')
+    tasks = Tarefas.objects.all()
+    total_tasks = len(tasks)
+    completed_tasks = 0
+    remaining_tasks = 0
+    pct = 0
+
+    for task in tasks:
+        if task.feita == True:
+            completed_tasks += 1
+
+
+    remaining_tasks = total_tasks - completed_tasks
+
+
+    if total_tasks >= 1:
+        pct = (completed_tasks/total_tasks) * 100
+
+
+
+    return render(
+        request, 
+        'tarefas/home.html',
+        {
+            'tasks': tasks,
+            'total_tasks': total_tasks,
+            'completed_tasks': completed_tasks,
+            'remaining_tasks': remaining_tasks,
+            'pct': pct
+
+        })
+
+def add(request):
+    form = TarefasForm(request.POST)
+    
+    if form.is_valid():
+        form.save()
+    return redirect('home')
